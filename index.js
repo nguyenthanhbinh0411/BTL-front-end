46da689bdb3f05bd7c37bcfd30ed3be2526fe961
huuhuy2910 <131759541+huuhuy2910@users.noreply.github.com>
2023-12-07 15:49:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,9 @@
 $(document).ready(function () {
-  // Các sự kiện và xử lý khác ...
-
-  // Sự kiện click cho nút Xem Danh Sách Người Dùng
-  $('#openUserListButton').click(function () {
-    if (isLoggedIn()) {
-      var users = JSON.parse(localStorage.getItem('users')) || [];
-      var currentUser = getCurrentUser();
-
-      if (currentUser && currentUser.role === 'admin') {
-        // Nếu người dùng là admin, hiển thị modal
-        $('#userListModal').modal('show');
-      } else {
-        // Người dùng không phải là admin, có thể hiển thị thông báo hoặc thực hiện hành động khác
-        console.log("Bạn không có quyền xem danh sách người dùng.");
-      }
-    } else {
-      // Người dùng chưa đăng nhập, bạn có thể chuyển hướng đến trang đăng nhập hoặc hiển thị modal đăng nhập
-      console.log("Người dùng chưa đăng nhập. Hiển thị modal đăng nhập hoặc chuyển hướng đến trang đăng nhập.");
-    }
-  });
-
   // Sự kiện click cho nav-item.dropdown
   $('.nav-item.dropdown').click(function (e) {
     e.preventDefault();
+
+    // Hiển thị hoặc ẩn dropdown-menu
     $(this).find('.dropdown-menu').slideToggle();
   });
 
@@ -30,6 +11,7 @@ $(document).ready(function () {
 
   $('.btn-show-more').click(function () {
     hiddenProducts.slice(0, 3).fadeIn();
+
     hiddenProducts = hiddenProducts.slice(3);
 
     if (hiddenProducts.length === 0) {
@@ -70,10 +52,10 @@ $(document).ready(function () {
       // Kiểm tra vai trò của người dùng
       if (user.role === 'admin') {
         // Nếu là admin, hiển thị nút Xem Danh Sách Người Dùng
-        $('#openUserListButton').show();
+        $('.dropdown-menu').html('<li class="logout-btn"><a class="dropdown-item" href="#" onclick="logout()">Đăng Xuất</a></li><li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#userListModal">Xem Danh Sách Người Dùng</a></li>');
       } else {
         // Nếu là user, ẩn nút Xem Danh Sách Người Dùng
-        $('#openUserListButton').hide();
+        $('.dropdown-menu').html('<li class="logout-btn"><a class="dropdown-item" href="#" onclick="logout()">Đăng Xuất</a></li>');
       }
 
       // Có thể thực hiện các hành động khác sau khi đăng nhập thành công
@@ -120,28 +102,22 @@ $(document).ready(function () {
 
   // Xử lý sự kiện click cho nút Xem Danh Sách Người Dùng
   $('#userListModal').on('show.bs.modal', function () {
-    if (isLoggedIn()) {
-      // Lấy danh sách người dùng từ localStorage
-      var users = JSON.parse(localStorage.getItem('users')) || [];
+    // Lấy danh sách người dùng từ localStorage
+    var users = JSON.parse(localStorage.getItem('users')) || [];
 
-      // Hiển thị danh sách người dùng trong bảng
-      var userListBody = $('#userListBody');
-      userListBody.empty(); // Xóa dữ liệu cũ
+    // Hiển thị danh sách người dùng trong bảng
+    var userListBody = $('#userListBody');
+    userListBody.empty(); // Xóa dữ liệu cũ
 
-      if (users.length > 0) {
-        users.forEach(function (user, index) {
-          userListBody.append('<tr><th scope="row">' + (index + 1) + '</th><td>' + user.username + '</td><td>' + user.password + '</td><td>' + user.role + '</td><td><button class="btn btn-danger btn-sm" onclick="deleteUser(' + index + ')">Xóa</button></td></tr>');
-        });
-      } else {
-        // Không có người dùng
-        userListBody.append('<tr><td colspan="5">Không có người dùng.</td></tr>');
-      }
+    if (users.length > 0) {
+      users.forEach(function (user, index) {
+        userListBody.append('<tr><th scope="row">' + (index + 1) + '</th><td>' + user.username + '</td><td>' + user.password + '</td><td>' + user.role + '</td><td><button class="btn btn-danger btn-sm" onclick="deleteUser(' + index + ')">Xóa</button></td></tr>');
+      });
     } else {
-      // Người dùng chưa đăng nhập
-      console.log("Người dùng chưa đăng nhập. Không thể xem danh sách người dùng.");
+      // Không có người dùng
+      userListBody.append('<tr><td colspan="5">Không có người dùng.</td></tr>');
     }
   });
-
   // Ẩn modal
   $('#LGcloseButton').click(function () {
     $('#loginModal').modal('hide');
@@ -154,26 +130,37 @@ $(document).ready(function () {
   });
 });
 
-// Hàm kiểm tra trạng thái đăng nhập
-function isLoggedIn() {
-  // Thực hiện logic xác thực của bạn ở đây
-  return true; // Trả về true nếu người dùng đã đăng nhập, ngược lại trả về false
+// Hàm lưu thông tin tài khoản vào localStorage
+function saveCredentials(username, password) {
+  // Kiểm tra xem đã có danh sách người dùng trong localStorage chưa
+  var users = JSON.parse(localStorage.getItem('users')) || [];
+
+  // Thêm thông tin tài khoản mới vào danh sách
+  users.push({ username: username, password: password });
+
+  // Lưu danh sách người dùng vào localStorage
+  localStorage.setItem('users', JSON.stringify(users));
 }
 
-// Hàm lấy thông tin người dùng hiện tại từ local storage
-function getCurrentUser() {
-  var username = ''; // Lấy thông tin người dùng hiện tại dựa trên logic của bạn
+// Hàm xóa tài khoản
+function deleteUser(index) {
+  // Lấy danh sách người dùng từ localStorage
   var users = JSON.parse(localStorage.getItem('users')) || [];
-  return users.find(function (u) {
-    return u.username === username;
-  });
+
+  // Xóa người dùng khỏi danh sách
+  users.splice(index, 1);
+
+  // Lưu danh sách người dùng vào localStorage
+  localStorage.setItem('users', JSON.stringify(users));
+
+  // Hiển thị lại danh sách người dùng trong modal
+  $('#userListModal').modal('show');
 }
 
 // Hàm đăng xuất
 function logout() {
   // Ẩn nút Đăng Xuất, hiển thị nút Đăng Nhập và Đăng Ký
-  $('#openUserListButton').hide();
+  $('.dropdown-menu').html('<li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#loginModal">Đăng Nhập</a></li><li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#signupModal">Đăng Ký</a></li><li class="logout-btn" style="display:none;"><a class="dropdown-item" href="#" onclick="logout()">Đăng Xuất</a></li><li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#userListModal">Xem Danh Sách Người Dùng</a></li>');
 
   // Ẩn biểu tượng người dùng và hiển thị lại biểu tượng mặc định
-  $('.nav-link.dropdown-toggle').html('<i class="bi bi-person-circle"></i>');
-}
+  $('.nav-link.dropdown-toggle').html('<i class="bi bi-person-circle"></i>');}
